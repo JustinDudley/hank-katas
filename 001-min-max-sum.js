651a768781336be9ae653441a2c94aa2324b5014
Justin Dudley <justindudley99@gmail.com>
2022-06-29 14:20:27 -0400

    final

--- 001-min-max-sum.js
+++ 001-min-max-sum.js
@@ -1,28 +1,12 @@
 
-
 function miniMaxSum(arr) {
+    let tempArr = arr.sort()
+    const minSum = tempArr.slice(0, -1).reduce(function(a, b) { return a + b; }, 0);
+    const maxSum = tempArr.slice(1, 5).reduce(function(a, b) { return a + b; }, 0)
 
-let tempArr = arr.sort()
-
-// console.log(tempArr)
-// console.log(tempArr.slice(0, -1)).reduce(function(a, b) { return a + b; }, 0);
-// console.log([1,2,3].reduce(function(a, b) { return a + b; }, 0)  )
-// console.log(tempArr.slice(0, -1))
-
-
-console.log(tempArr.slice(0, -1).reduce(function(a, b) { return a + b; }, 0))
-console.log(tempArr.slice(0, 5).reduce(function(a, b) { return a + b; }, 0))
-
-const minSum = tempArr.slice(0, -1).reduce(function(a, b) { return a + b; }, 0);
-const maxSum = tempArr.slice(0, 5).reduce(function(a, b) { return a + b; }, 0)
-
-console.log(String(minSum) + ' ' + String(maxSum))
-
-
-
-// console.log(tempArr.splice(-1,1).reduce(function(a, b) { return a + b; }, 0))
+    console.log(String(minSum) + ' ' + String(maxSum))
 }
 
 const arr = [2,3,1,4,5];
-    miniMaxSum(arr);
+miniMaxSum(arr);
 
